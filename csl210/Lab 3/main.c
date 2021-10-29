91fbb5d421ef3924c17e9cda6c15dd34c4dff8ad
Aviral Verma <bt20cse214@iiitn.ac.in>
2021-10-29 10:49:33 +0530

    Added my outputs for testcases

--- csl210/Lab 3/main.c
+++ csl210/Lab 3/main.c
@@ -39,12 +39,105 @@ void printInorder(btNode_t* node)
 
 int main()
 {
-    char input[]="qwertyqqwfrtggggbryyyyyyyeeeeqqqqqqq";
+    char input[] = {'a','b','c','d','g','h','i','d','c','b','a','k','g','i','d','a','c','c','r','s','a','b','i','h','r','r','r','d','c','s'};
+    // char input[]="qwertyqqwfrtggggbryyyyyyyeeeeqqqqqqq";
     // char input[] = "abc";
-    unsigned short sz = strlen(input);
+    unsigned short sz = 30;
     btNode_t *root = createHuffmanTree(input, sz);
     printTree(root, 5);
     printInorder(root);
 }
 
 //Aviral Verma, BT20CSE214, IIITN
+
+/* My Output 1:
+
+                    c, 5
+
+               , 9      
+
+                    r, 4
+
+          , 17
+
+                    d, 4
+
+               , 8      
+
+                    a, 4
+
+     , 30
+
+                         s, 2
+
+                    , 4
+
+                         h, 2
+
+               , 7
+
+                    i, 3
+
+          , 13
+
+                    b, 3
+
+               , 6
+
+                         g, 2
+
+                    , 3
+
+                         k, 1
+1->3->2->6->3->13->3->7->2->4->2->30->4->8->4->17->4->9->5->
+
+My Output 2:
+
+               q, 10
+
+          , 20
+
+                    e, 5
+
+               , 10
+
+                         r, 3
+
+                    , 5
+
+                         t, 2
+
+     , 36
+
+               y, 8
+
+          , 16
+
+                    g, 4
+
+               , 8
+
+                         w, 2
+
+                    , 4
+
+                              b, 1
+
+                         , 2
+
+                              f, 1
+1->2->1->4->2->8->4->16->8->36->2->5->3->10->5->20->10->
+
+My Output 3:
+
+               b, 1
+
+          , 2
+
+               a, 1
+
+     , 3
+
+          c, 1
+1->3->1->2->1->
+*/
